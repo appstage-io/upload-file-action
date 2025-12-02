d998ef6749dd1a33a0674572d2078cc9730ca8a0
Mike Howson <mike.howson@p4innovation.com>
2025-12-02 12:06:00 +0000

    Updated to add direct CDN upload

--- index.js
+++ index.js
@@ -2,35 +2,94 @@ const core = require("@actions/core");
 const axios = require("axios");
 const https = require('https');
 const fs = require('fs/promises');
-const FormData = require('form-data');
+const crypto = require('crypto');
+const path = require('path');
 
 const token = core.getInput('token');
 const filename = core.getInput('filename');
 const host = core.getInput('host') || 'https://www.appstage.io';
 
+const instance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
+
+async function calculateChecksum(filepath) {
+  const fileBuffer = await fs.readFile(filepath);
+  const hashSum = crypto.createHash('md5');
+  hashSum.update(fileBuffer);
+  return hashSum.digest('base64');
+}
+
+async function requestDirectUpload(filename, fileSize, checksum) {
+  const { data } = await instance.post(
+    `${host}/api/direct_uploads`,
+    {
+      blob: {
+        filename: path.basename(filename),
+        byte_size: fileSize,
+        checksum: checksum,
+        content_type: 'application/octet-stream'
+      }
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+  );
+  return data;
+}
+
+async function uploadToCDN(filepath, directUploadData) {
+  const fileBuffer = await fs.readFile(filepath);
+  const uploadUrl = directUploadData.direct_upload.url;
+  const uploadHeaders = directUploadData.direct_upload.headers;
+
+  await axios.put(uploadUrl, fileBuffer, {
+    headers: uploadHeaders,
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
+  });
+}
+
+async function createReleaseFile(signedBlobId) {
+  const { data } = await instance.post(
+    `${host}/api/live_builds`,
+    {
+      signed_blob_id: signedBlobId
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+  );
+  return data;
+}
+
 (async () => {
   try {
-    const instance = axios.create({
-      httpsAgent: new https.Agent({  
-        rejectUnauthorized: false
-      })
-    });
-
     console.log(`Uploading ${filename}...`)
 
-    const file = await fs.readFile(filename);
-    let formData = new FormData();
-    formData.append("release_file[cloud_stored_file]", file, filename);
+    const stats = await fs.stat(filename);
+    const fileSize = stats.size;
 
-    const { data } = await instance.post(`${host}/api/live_builds`, formData,{
-      headers: {
-          "Content-Type": "multipart/form-data",
-          "Accept": "application/json",
-          'Authorization': `Bearer ${token}`
-      }
-    });
-    console.log(`File ${data.display_name} uploaded`)
-    core.setOutput("file", data);
+    const checksum = await calculateChecksum(filename);
+    const directUploadData = await requestDirectUpload(filename, fileSize, checksum);
+
+    await uploadToCDN(filename, directUploadData);
+
+    const releaseFile = await createReleaseFile(directUploadData.signed_id);
+
+    console.log(`File ${releaseFile.display_name} uploaded`)
+    core.setOutput("file", releaseFile);
   } catch (error) {
     console.log(`File upload failed - ${error.message}`)
     core.setFailed(`File upload failed - ${error.message}`);
